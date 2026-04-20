dac63cc91497eb8a436ac72bc3b939931532ad63
Grek1785 <thanhdatya1785@gmail.com>
2026-04-20 21:54:07 +0700

    feat: class, attendance

---
+++ src/routes/class.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const classController = require('../controllers/class.controller');
+
+// Lấy danh sách tất cả lớp học
+router.get('/', classController.getClasses);
+
+// Tạo lớp học mới
+router.post('/', classController.createClass);
+
+// Lấy thông tin 1 lớp học cụ thể
+router.get('/:id', classController.getClassById);
+
+// Lấy danh sách học sinh của 1 lớp
+router.get('/:id/students', classController.getClassStudents);
+
+module.exports = router;
