af8083f864b6787e4fa907fad43a3274d252edf5
Grek1785 <thanhdatya1785@gmail.com>
2026-04-20 22:02:41 +0700

    update: up Auth for attendance, class

--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const classController = require('../controllers/class.controller');
-
+const { requireAuth, requireRole } = require('../middlewares/auth.middleware')
 // Lấy danh sách tất cả lớp học
 router.get('/', classController.getClasses);
 
 // Tạo lớp học mới
-router.post('/', classController.createClass);
+router.post('/', requireAuth,requireRole('admin', 'teacher'), classController.createClass);
 
 // Lấy thông tin 1 lớp học cụ thể
-router.get('/:id', classController.getClassById);
+router.get('/:id', requireAuth,requireRole('admin','teacher'), classController.getClassById);
 
 // Lấy danh sách học sinh của 1 lớp
-router.get('/:id/students', classController.getClassStudents);
+router.get('/:id/students', requireAuth,requireRole('admin', 'teacher'), classController.getClassStudents);
 
 module.exports = router;
